cf17dce5a52a288a5fa4505960fc76d92aa69257
Andy Donnell <00.disk-turbo@icloud.com>
2023-12-21 05:38:59 +0000

    Fix deadlock

--- wip/pipeline/main.go
+++ wip/pipeline/main.go
@@ -3,64 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-	"math/rand"
-	"time"
 )
 
-// customContext carries both the standard context and an error channel.
-type customContext struct {
-	context.Context
-	errChan    chan error
-	cancelFunc context.CancelFunc
-}
-
-// newCustomContext creates a new customContext with an error channel.
-func newCustomContext(ctx context.Context) *customContext {
-	cancelCtx, cancelFunc := context.WithCancel(ctx)
-	return &customContext{
-		Context:    cancelCtx,
-		errChan:    make(chan error, 1),
-		cancelFunc: cancelFunc,
-	}
-}
-
-// cancelWithError cancels the context and sends an error to the error channel.
-func (c *customContext) cancelWithError(err error) {
-	select {
-	case c.errChan <- err:
-	default: // Avoid blocking if an error is already set.
-	}
-	c.cancelFunc()
-}
-
-// generateNumbers sends numbers into a channel, simulating potential errors.
-func generateNumbers(ctx *customContext, max int) <-chan int {
+// filterEvenNumbers filters out even numbers.
+func filterEvenNumbers(ctx context.Context, numbers []int) <-chan int {
 	out := make(chan int)
 	go func() {
 		defer close(out)
-		for i := 1; i <= max; i++ {
+		for idx, n := range numbers {
 			select {
 			case <-ctx.Done():
 				return
-			case out <- i:
-			}
-		}
-	}()
-	return out
-}
-
-// filterEvenNumbers filters out even numbers.
-func filterEvenNumbers(ctx *customContext, in <-chan int) <-chan int {
-	out := make(chan int)
-	go func() {
-		defer close(out)
-		for n := range in {
-			if n%2 != 0 {
-				select {
-				case <-ctx.Done():
-					return
-				case out <- n:
+			default:
+				fmt.Println(idx, ": ", n)
+				if n%2 != 0 {
+					out <- n
 				}
 			}
 		}
@@ -69,64 +26,39 @@ func filterEvenNumbers(ctx *customContext, in <-chan int) <-chan int {
 }
 
 // sumNumbers receives numbers and sums them.
-func sumNumbers(ctx *customContext, in <-chan int) <-chan int {
-	out := make(chan int, 1) // Buffered channel to send the sum
+func sumNumbers(ctx context.Context, in <-chan int) <-chan int {
+	out := make(chan int)
 	go func() {
 		defer close(out)
 		sum := 0
 		for n := range in {
 			sum += n
 		}
-		out <- sum // Send sum after processing all numbers or if the loop is exited early
+		out <- sum
 	}()
 	return out
 }
 
-// newPipeline sets up the pipeline and returns a function to start it with an int argument.
-func newPipeline(ctx *customContext) func(int) <-chan int {
-	return func(max int) <-chan int {
-		firstStage := generateNumbers(ctx, max)
-		secondStage := filterEvenNumbers(ctx, firstStage)
-		sumStage := sumNumbers(ctx, secondStage)
-		return sumStage
-	}
-}
-
-// monitorPipeline monitors the pipeline and returns the calculated sum and any error that occurs.
-func monitorPipeline(ctx *customContext, sumStage <-chan int) (int, error) {
-	var pipelineErr error
-	done := make(chan struct{}) // Signal completion of error handling goroutine
-
-	go func() {
-		select {
-		case err := <-ctx.errChan:
-			pipelineErr = err
-			ctx.cancelFunc()
-		case <-ctx.Done():
-		}
-		close(done) // Signal completion
-	}()
-
+// monitorPipeline monitors the pipeline and returns the calculated sum.
+func monitorPipeline(ctx context.Context, sumStage <-chan int) int {
 	sum := 0
-	for val := range sumStage {
-		sum += val
+	for {
+		select {
+		case val, ok := <-sumStage:
+			if !ok {
+				return sum
+			}
+			sum += val
+		case <-ctx.Done():
+			return sum
+		}
 	}
-
-	<-done // Wait for the error handling goroutine to signal completion
-	return sum, pipelineErr
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	ctx := newCustomContext(context.Background())
-	defer ctx.cancelFunc()
-
-	startPipeline := newPipeline(ctx)
-
-	sum, err := monitorPipeline(ctx, startPipeline(10))
-	if err != nil {
-		log.Printf("Pipeline error: %v", err)
-	} else {
-		fmt.Println("Sum of odd numbers:", sum)
-	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	pipeline := sumNumbers(ctx, filterEvenNumbers(ctx, []int{3, 4, 6, 1, 22, 59, 784, 32, 121}))
+	fmt.Println("Expected:", 3+1+59+121)
+	fmt.Println("Result:", monitorPipeline(ctx, pipeline))
 }
---
+++ wip/pipeline/tmp.go
@@ -0,0 +1,3 @@
+package main
+
+// ... [customContext and other pipeline stage functions remain unchanged] ...
